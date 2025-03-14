f0847b32c2cada0b48b3f82e16642395a0653349
Dorian Cottle <dorian.cottle@students.snow.edu>
2025-03-14 11:36:49 -0600

    first

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing.Text;
+using Akka.Hosting;
+using Akka.Streams;
+using akkastreamsinclass;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using static akkastreamsinclass.TransformerActor;
+CancellationTokenSource _shutdownCts = new();
+
+var hostBuilder = new HostBuilder();
+
+hostBuilder.ConfigureServices((context, services) =>
+{
+    services.AddAkka("MyActorSystem", (builder, sp) =>
+    {
+        builder
+            .WithActors((system, registry, resolver) =>
+            {
+                var helloActor = system.ActorOf(Props.Create(() => new TransformerActor()), "transformer");
+                registry.Register<TransformerActor>(helloActor);
+            });
+    });
+});
+
+var host = hostBuilder.Build();
+
+var completionTask = host.RunAsync();
+
+// grab the ActorSystem from the DI container
+var system = host.Services.GetRequiredService<ActorSystem>();
+
+// grab the ActorRef from the DI container
+IActorRef transformer = host.Services.GetRequiredService<IRequiredActor<TransformerActor>>().ActorRef;
+
+var source = Source.ActorRef<int>(100, OverflowStrategy.DropHead);
+var (srcActor, src) = source.PreMaterialize(system);
+
+//subscribes
+transformer.Tell(new BroadcastSubscriptionRequest(srcActor));
+
+await foreach (var number in src
+                    .RunAsAsyncEnumerable(system)
+                    .WithCancellation(_shutdownCts.Token))
+{
+    Console.WriteLine(number);
+}
+
+// shut the actor down to prevent resource leaks
+srcActor.Tell(PoisonPill.Instance);
+
+await completionTask; // wait for the host to shut down
---
+++ TransformerActor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using Akka.Util.Internal;
+
+namespace akkastreamsinclass;
+
+public record BroadcastRequest();
+public record BroadcastSubscriptionRequest(IActorRef subscriber);
+public class TransformerActor : ReceiveActor, IWithTimers
+{
+    public ITimerScheduler Timers {get; set;}
+    private ImmutableHashSet<IActorRef> subscribers = [];
+    private int number = 0;
+    public TransformerActor()
+    {
+        Receive<string>(str =>
+        {
+            Sender.Tell(str.ToUpperInvariant());
+        });
+        Receive<BroadcastRequest>(m => {
+            number++; //no concurrency issues because single actors run single threadedly. 
+            subscribers.ForEach(s => s.Tell(number));
+        });
+        Receive<BroadcastSubscriptionRequest>(m => {
+            subscribers = [..subscribers, m.subscriber];
+        });
+    }
+
+    protected override void PreStart()
+    {
+        Timers.StartPeriodicTimer("thing", new BroadcastRequest(), TimeSpan.FromSeconds(1));
+    }
+}
